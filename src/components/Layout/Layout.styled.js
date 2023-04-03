246d0218839e0ac67baa85dfbd61ff2ae5cbe8c8
basesnel <basesnel@gmail.com>
2023-04-03 14:24:55 +0300

    styled components in folders

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -10,19 +10,20 @@ export const Container = styled.div`
 `;
 
 export const Header = styled.header`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: 12px;
   padding: 8px 16px;
   margin-bottom: 16px;
   /* border-bottom: 1px solid black; */
   box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.2), 0 4px 5px 0 rgba(0, 0, 0, 0.14),
     0 1px 10px 0 rgba(0, 0, 0, 0.12);
 
+  nav {
+    max-width: 960px;
+    margin: 0 auto;
+  }
+
   nav > ul {
     display: flex;
-    gap: 10px;
+    gap: 12px;
     padding-left: 0;
     list-style: none;
   }
